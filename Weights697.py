47ad80b3aa46cdb09bed076e0f8163d1c29cbf9e
ADEEP REDDY <adeepreddyvelagala@gmail.com>
2025-02-03 23:43:40 +0530

    Create Weights697.py

---
+++ Weights697.py
@@ -0,0 +1,19 @@
+# Chef is playing with weights. He has an object weighing W
+# W units. He also has three weights each of X,Y and Z units respectively.
+# Help him determine whether he can measure the exact weight of the object with one or more of these weights.
+
+# If it is possible to measure the weight of object with one or more of these weights, print YES, otherwise print NO.
+
+# Input Format
+# The first line of input will contain a single integer T, denoting the number of test cases.
+# Each test case consists of single line containing a four positive integers W,X,Y, and Z.
+# Output Format
+
+# For each test case, output on a new line YES if it is possible to measure the weight of object with one or more of these weights, otherwise print NO.
+# You may print each character of the string in either uppercase or lowercase (for example, the strings yes, YES, Yes, and yeS will all be treated as identical).
+for r in range(int(input())):
+  w,x,y,z=map(int,input().split())
+  if w==x or w==y or w==z or w==(x+y+z) or w==(x+y) or w==(y+z) or w==(z+x):
+    print("YES")
+  else:
+    print("NO")
